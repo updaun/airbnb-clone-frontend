711ef6ebb8871a80ef95d8c0d76516a05bb5461b
updaun <updauney@daum.net>
2023-04-17 23:27:22 +0900

    #17.3 Router Setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { RouterProvider } from 'react-router-dom';
+import router from "./router";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,7 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <RouterProvider router={router}/>
     </ChakraProvider>
   </React.StrictMode>
 );
