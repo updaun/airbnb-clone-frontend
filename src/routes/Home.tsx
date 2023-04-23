88d42f15e7901a9774173222c97d346673e293fc
updaun <updauney@daum.net>
2023-04-23 23:58:02 +0900

    #18.4 Rooms Grid Complete

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,4 +1,5 @@
-import { Box, Grid, Heading, Image, VStack, Text } from "@chakra-ui/react";
+import { Box, Grid, Heading, Image, VStack, Text, HStack } from "@chakra-ui/react";
+import { FaStar } from "react-icons/fa";
 
 export default function Home() {
     return (
@@ -9,10 +10,18 @@ export default function Home() {
                     h="280"
                     src="https://a0.muscache.com/im/pictures/prohost-api/Hosting-607458038229062130/original/1e20dfc7-ea12-44b2-a837-2bdcd8502133.jpeg?im_w=720" />
             </Box>
-            <Heading noOfLines={1} fontSize={"md"}>
-                Ganggu-myeon, Yeongdeok-gun, 경상북도, 한국
-                </Heading>
+            <Box>
+                <Grid gap={2} templateColumns={"6fr 1fr"}>
+                    <Text as="b" noOfLines={1} fontSize={"md"}>
+                        Ganggu-myeon, Yeongdeok-gun, 경상북도, 한국
+                    </Text>
+                    <HStack spacing={1}>
+                        <FaStar size={15}/>
+                        <Text>5.0</Text>    
+                    </HStack>    
+                </Grid>
                 <Text fontSize={"sm"} color="gray.600">Seoul, S. Korea</Text>
+            </Box>
                 <Text fontSize={"sm"} color="gray.600">
                     <Text as="b">$72</Text>/ night
                 </Text>
