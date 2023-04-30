59110dd01bb9281209265aca5735ee63259dc133
updaun <updauney@daum.net>
2023-04-30 23:41:28 +0900

    #19.6 Photos Grid

--- src/routes/RoomDetail.tsx
+++ src/routes/RoomDetail.tsx
@@ -1,14 +1,48 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { getRoom } from "./api";
+import { IRoomDetail } from "../types";
+import { Box, Grid, GridItem, Heading, Image, Skeleton } from "@chakra-ui/react";
 
 export default function RoomDetail() {
     const { roomPk } = useParams();
-    const { isLoading, data } = useQuery(["rooms", roomPk], getRoom)
+    const { isLoading, data } = useQuery<IRoomDetail>(["rooms", roomPk], getRoom)
     console.log(data);
     return (
-        <div>
-            RoomDetail
-        </div>
+        <Box
+            mt={10}
+            px={{
+                base: 10,
+                lg: 40,
+            }}
+        >
+            <Skeleton height={"43px"} width={"25%"} isLoaded={!isLoading}>
+                <Heading>{ data?.name }</Heading>
+            </Skeleton>
+            <Grid
+                mt={8}
+                rounded={"xl"}
+                overflow={"hidden"}
+                gap={3}
+                height="60vh"
+                templateRows={"repeat(2, 1fr)"}
+                templateColumns={"repeat(4, 1fr)"}
+            >
+                {[0, 1, 2, 3, 4].map((index) => (
+                    <GridItem
+                        colSpan={ index === 0 ? 2 : 1 }
+                        rowSpan={ index === 0 ? 2 : 1 }
+                        overflow={"hidden"} key={index}>
+                        <Skeleton isLoaded={!isLoading} h="100%" w="100%">
+                            <Image
+                                w="100%"
+                                h="100%"
+                                objectFit={"cover"}
+                                src={data?.photos[index]?.file} />
+                       </Skeleton>
+                    </GridItem>
+                ))}
+            </Grid>
+        </Box>
     )
 }
