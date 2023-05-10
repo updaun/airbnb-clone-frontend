86945dd700984764e1cd3105cd633f6564700012
updaun <updauney@daum.net>
2023-05-10 17:15:07 +0900

    #20.8 Emails

--- src/routes/GithubConfirm.tsx
+++ src/routes/GithubConfirm.tsx
@@ -1,15 +1,29 @@
-import { Heading, Text, VStack, Button, Spinner } from "@chakra-ui/react";
+import { Heading, Text, VStack, Button, Spinner, useToast } from "@chakra-ui/react";
 import { useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { githubLogIn } from "./api";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function GithubConfirm() {
     const { search } = useLocation();
+    const toast = useToast();
+    const queryClient = useQueryClient();
+    const navigate = useNavigate(); 
     const confirmLogin = async() => {
         const params = new URLSearchParams(search);
         const code = params.get("code")
         if (code) {
-            await githubLogIn(code);
+            const status = await githubLogIn(code);
+            if (status === 200) {
+                toast({
+                    status: "success",
+                    title: "Welcome!",
+                    position: "bottom-right",
+                    description: "Happy to have you back!",
+                });
+                queryClient.refetchQueries(["me"]);
+                navigate("/");
+            }
         }
     }
     useEffect(() => {
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -4,9 +4,13 @@ import RoomSkeleton from "../components/RoomSkeleton";
 import { useQuery } from "@tanstack/react-query";
 import { getRooms } from "./api";
 import { IRoomList } from "../types";
+import { useEffect } from "react";
 
 export default function Home() {
     const { isLoading, data } = useQuery<IRoomList[]>(["rooms"], getRooms);
+    useEffect(() => {
+        console.log("heello");
+    }, []);
     return (
         <Grid mt={10} px={{ base:10, lg:40}} columnGap={4} rowGap={8} templateColumns={{
             sm: "1fr",
